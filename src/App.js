027c5cea0416a5789f8c69f107113723e7a6d013
Joaquin Zabaschi <zabaschijoaquin@gmail.com>
2022-03-22 21:05:15 -0300

    Changes to be committed: 	modified:   src/App.js 	modified:   src/app.css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import './app.css';
 import Header from './components/Header/header';
 import Nav from './components/Nav/nav';
-import Producto1 from './components/Productos/Producto1/producto1';
-import Producto2 from './components/Productos/Producto2/producto2';
-import Producto3 from './components/Productos/Producto3/producto3';
-import Producto4 from './components/Productos/Producto4/producto4';
-import Producto5 from './components/Productos/Producto5/producto5';
-import Producto6 from './components/Productos/Producto6/producto6';
+import Productos from './components/Productos/productos';
 import Footer from './components/Footer/footer';
 
-function App() {
+const App = () => {
   return (
       <div className='app'>
         <div className='header'>
@@ -19,22 +14,7 @@ function App() {
           <Nav/> 
         </div>
         <div className="producto" id='producto1'>
-          <Producto1/>
-        </div >
-        <div className="producto" id='producto2'>
-          <Producto2/>
-        </div>
-        <div className="producto" id='producto3'>
-          <Producto3/>
-        </div> 
-        <div className="producto" id='producto4'>
-          <Producto4/>
-        </div>
-        <div className="producto" id='producto5'>
-          <Producto5/>
-        </div>
-        <div className="producto" id='producto6'>
-          <Producto6/>
+          <Productos/>
         </div>
         <div className='footer'>
           <Footer/>
